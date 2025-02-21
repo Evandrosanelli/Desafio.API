e94832e5ac47a33855dca1f8a71df1cc3585c57c
evandro <evandrosanelli@gmail.com>
2025-02-20 21:47:49 -0300

    Adicionando conexão com o banco de dados postgreSQL

--- Desafio.API/Program.cs
+++ Desafio.API/Program.cs
@@ -1,15 +1,17 @@
+using Desafio.API.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<PostgreContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,7 +19,5 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
---
+++ Desafio.API/Repositories/PostgreContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Desafio.API.Repositories
+{
+    public class PostgreContext : DbContext
+    {
+        public PostgreContext(DbContextOptions<PostgreContext> options) : base(options) { }
+
+        #region Tabelas
+        #endregion
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=banco;Username=docker;Password=docker");
+            }
+        }
+    }
+}
